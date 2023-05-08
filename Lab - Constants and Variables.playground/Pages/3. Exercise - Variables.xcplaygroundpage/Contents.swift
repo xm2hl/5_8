05aef9125ca7a3f3ab2cbc0f1f81da1e8b1d28f0
mamdouh al rashidi <mamdouh_m@outlook.sa>
2023-05-08 08:20:53 +0300

    ال،اجب

---
+++ Lab - Constants and Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+/*:
+## Exercise - Variables
+ 
+ Declare a variable `schooling` and set it to the number of years of school that you have completed. Print `schooling` to the console.
+ */
+var study_yesrs = 16
+print(study_yesrs)
+
+//:  Now imagine you just completed an additional year of school, and update the `schooling` variable accordingly. Print `schooling` to the console.
+study_yesrs += 1
+print(study_yesrs)
+
+//:  Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the `print` function.
+print(" let value cannot be modified  , var the value can be modified ")
+
+/*:
+[Previous](@previous)  |  page 3 of 10  |  [Next: App Exercise - Step Count](@next)
+ */
